8ce59dc0207b9487ecd61503720d4cd18c345574
giulio pascolat <Giuliopascolat@gmail.com>
2025-01-14 14:30:05 +0100

    test artifacts

--- TestProject1/OperationsTest.cs
+++ TestProject1/OperationsTest.cs
@@ -5,20 +5,21 @@ namespace TestProject1;
 public class OperationsTest
 {
     private readonly Operations _operations = new();
+
     [Theory]
-    [InlineData(1,1)]
-    [InlineData(3,5)]
-    [InlineData(3,0)]
+    [InlineData(1, 1)]
+    [InlineData(3, 5)]
+    [InlineData(3, 0)]
     public void Sum_Should_Work(int a, int b)
     {
         Assert.Equal(_operations.sum(a, b), a + b);
     }
-
-
-
-    [Fact]
-    public void Sum_Should_Not_Work()
-    {
-        Assert.Equal(-1, _operations.sum(1, 1));
-    }
 }
+
+//
+//     [Fact]
+//     public void Sum_Should_Not_Work()
+//     {
+//         Assert.Equal(-1, _operations.sum(1, 1));
+//     }
+// }
